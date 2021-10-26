151568c293bb778faa6a9e301929919934d70c59
zujjainwala <zakirlu2@illinois.edu>
2021-10-26 18:37:23 -0500

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -3,11 +3,20 @@ def get_sequences(database, min_sup):
     with open(database) as file_object:
         lines = file_object.readlines()
 
+    min_support = min_sup
     for line in lines:
         begin = line.find("<") + len("<")
         end = line.find(">")
         substring = line[begin:end]
-        print(substring)
+
+        F1 = {}
+        count = 0
+        for str in substring:
+            if (str == 'a'):
+                count += 1
+
+        #print(substring)
+        print(count)
     
 
 get_sequences('test.txt', 4)
