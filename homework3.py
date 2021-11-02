cf675d878f1c26d37a11aa69efc74d347e907db2
zujjainwala <55063927+zujjainwala@users.noreply.github.com>
2021-11-01 22:43:43 -0500

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -2,8 +2,9 @@ def get_sequences(file, min_sup):
 
     with open(file) as file_object:
         lines = file_object.readlines()
+    #lines = open(file)
 
-    F1 = {}
+    F1 = dict()
     for line in lines:
         begin = line.find("<") + len("<")
         end = line.find(">")
@@ -11,10 +12,10 @@ def get_sequences(file, min_sup):
 
         # Finding the 1-frequent itemset
         for str in substring:
-            if str in F1:
-                F1[str] = F1[str] + 1
-            else:
+            if str not in F1:
                 F1[str] = 1
+            else:
+                F1[str] += 1
     
     print(F1)
 
@@ -25,7 +26,6 @@ def get_sequences(file, min_sup):
 
     print(F1)
 
-
     # Finding the 2-frequent itemset
     ## Want to loop through the dictionary and make all possible
     ## combinations between the keys.
@@ -33,12 +33,12 @@ def get_sequences(file, min_sup):
     ## and determine if these combinations are in each of the substrings.
     # for key, value in F1.items():
     #     return 0
-    F2 = {}
+    F2 = dict()
     altered_input = list(F1)
     result = [(x,y) for idx, x in enumerate(altered_input) for y in altered_input[idx + 1: ]]
     result = [ x+y for (x, y) in result]
     
-    # print(result)
+    print(result)
     for x in result:
         for line in lines:
             begin = line.find("<") + len("<")
@@ -48,11 +48,12 @@ def get_sequences(file, min_sup):
             if x[0] in substring and x[1] in substring:
                 #print(substring.index(x[0]), substring.index(x[1]))
                 if substring.index(x[0]) < substring.index(x[1]):
-                    if x in F2:
-                        F2[x] = F2[x] + 1
-                    else:
+                    if x not in F2:
                         F2[x] = 1
-    #print(F2)
+                    else:
+                        F2[x] += 1
+    return(F2)
+
 
 # Testing with a simple database and minimum support
-get_sequences('test.txt', 2)
+#get_sequences('test.txt', 2)
